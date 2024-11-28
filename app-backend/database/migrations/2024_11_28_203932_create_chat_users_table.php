c61fb23dbb134885afd449ab0fa515be3789afa1
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-29 01:52:43 +0500

    Bug fixed

--- app-backend/database/migrations/2024_11_28_203932_create_chat_users_table.php
+++ app-backend/database/migrations/2024_11_28_203932_create_chat_users_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChatUserTable extends Migration
+class CreateChatUsersTable extends Migration
 {
     public function up()
     {
